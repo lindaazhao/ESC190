0e5259971b5260c19d3172fa8d573073eccce650
lindaazhao <linda.zhao23@gmail.com>
2022-01-12 16:14:49 -0500

    Mostly finish lab 1

--- lab1/lab1.c
+++ lab1/lab1.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <ctype.h>
 
+// Part 1: Splitting Bills
 double split_bill(double base_amount, double tax_rate, double tip_rate, int num_people) {
     float bill = (base_amount * (1 + tax_rate)) * (1 + tip_rate);
     float split_amount = roundf((bill / num_people) * 100)/100;
@@ -8,8 +12,64 @@ double split_bill(double base_amount, double tax_rate, double tip_rate, int num_
         split_amount += 0.01;
     }
     
-    // hi james!
-    // hi linda!
-    
     return split_amount;
 }
+
+// Question 1: 
+// The code truncates the number after the decimal point
+
+// Question 2: 
+/* First calculated raw split amount, then rounded it to nearest cent using roundf.
+ * Used an if statement to only round up if the intial split amount did not cover the bill. */
+ 
+ 
+ // Part 2: Adjust Pizza Prices
+ double adjust_price(double original_price) {
+     float new_price = 10 * pow(original_price, 0.5);
+     return new_price;
+ }
+ 
+ 
+ // Part 3: Picky Eating
+ int sandy_eats(char menu_item[]){   
+     // Currently only works for lowercase menu items
+     char *test_j;
+     char *test_k;
+     char *test_l;
+     char *test_fish;
+     
+     test_j = strstr(menu_item, "j");
+     test_k = strstr(menu_item, "k");
+     test_l = strstr(menu_item, "l");
+     test_fish = strstr(menu_item, "fish");
+     
+     int counter = 0;
+     for(int i = 0; i < strlen(menu_item); i++) {
+         if(menu_item[i] != ' ')
+             counter++;
+     }
+     
+     if(test_j || test_k || test_l || test_fish) {
+         return 0;
+     }
+     else if (counter % 2 == 1) {
+        return 0; 
+     }
+     else {
+         return 1;
+     }
+ }
+ 
+ 
+ // Part 4: Just Thinking About Fish
+ void imagine_fish(char thing[]){
+     char fish[] = "fish";
+     strcat(thing, fish);
+     return thing;
+ }
+ 
+ // Question 3
+ /* Using string.h, can use strcat to concatenate two strings (i.e. strcat(thing, "fish"))
+  * Without string.h, create a new string, then use a for/while loop to iterate through each 
+  * character of both strings and add them to the new string. */
+ 
--- lab1/test1.c
+++ lab1/test1.c
@@ -1,11 +1,65 @@
 #include <stdio.h> // need this to use the printf () function
 #include "lab1.h" // need this to link our Lab 1 functions
+#include <string.h>
 
 int main () {
     // Testing Part 1
     double test_p1 = split_bill(50.01 , 0.13 , 0.15 , 2);
-    printf ("The split will be : $%.2f.\n " , test_p1);
+    printf ("The split will be : $%.2f.\n" , test_p1);
     // $32.50
-    // Add more tests here ...
+    
+    double test_p2 = split_bill(50.01 , 0.13 , 0.15 , 4);
+    printf ("The split will be : $%.2f.\n" , test_p2);
+    // $16.25
+    
+    
+    // Testing Part 2
+    double test_p3 = adjust_price(25.00);
+    printf("The adjusted price is: $%.2f.\n", test_p3);
+    // Should be $50.00
+    
+    double test_p4 = adjust_price(45.67);
+    printf("The adjusted price is: $%.2f.\n", test_p4);
+    // Should be $67.57
+    
+    
+    // Testing Part 3
+    char test_food[] = "Three Cheese Pizza";
+    int test_p5 = sandy_eats(test_food);
+    if (test_p5 == 0) {
+        printf("Sandy would NOT eat '%s'.\n", test_food);
+    }
+    else {
+        printf("Sandy would eat '%s'.\n", test_food);
+    }
+    
+    char test_food2[] = "james";
+    int test_p6 = sandy_eats(test_food2);
+    if (test_p6 == 0) {
+        printf("Sandy would NOT eat '%s'.\n", test_food2);
+    }
+    else {
+        printf("Sandy would eat '%s'.\n", test_food2);
+    }
+    
+    char test_food3[] = "fish pizzaz";
+    int test_p7 = sandy_eats(test_food3);
+    if (test_p7 == 0) {
+        printf("Sandy would NOT eat '%s'.\n", test_food3);
+    }
+    else {
+        printf("Sandy would eat '%s'.\n", test_food3);
+    }
+    
+    
+    // Testing Part 4
+    char test_p8[30] = "zebra";
+    imagine_fish(test_p8);
+    printf("Just imagine, a %s!\n", test_p8);
+    // Just imagine, a zebrafish!
+    
+    
+    
+    
     return 0;
 }
