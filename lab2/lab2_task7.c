7034d0ca1641579627e060512e0db670aee7b1f1
lindaazhao <linda.zhao23@gmail.com>
2022-02-02 15:49:35 -0500

    Add a precautionary bracket

--- lab2/lab2_task7.c
+++ lab2/lab2_task7.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 
 void append(int** x, int* n, int val) {
-    *x = realloc(*x, (*n + 1) * sizeof(int));
+    *x = realloc(*x, ((*n) + 1) * sizeof(int));
     (*x)[*n] = val;
     *n += 1;
 }
