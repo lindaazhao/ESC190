1d84ab4bab96a1fafdc53851293eef11d0b02b33
lindaazhao <linda.zhao23@gmail.com>
2022-02-16 01:08:45 -0500

    Fix memory leaks

--- lab3/lab3.c
+++ lab3/lab3.c
@@ -1,42 +1,58 @@
 #include "lab3.h"
 #include <stdio.h>
+#include <string.h>
+
+// Helper functions
+void delete_node(struct party_node *node_to_delete){
+    free(node_to_delete->item);
+    free(node_to_delete->ta);
+    free(node_to_delete);
+}
+
+
+// Determine the length of a linked list
+int length_of_list(struct party_node **head){
+    int counter = 0;
+    struct party_node* temp_node = *head;
+    while (temp_node != NULL) {
+        counter++;
+        temp_node = temp_node->next;
+    }
+    return counter;
+}
+
+// ============================================================================
 
 // Add TA party item request to the list
 int add_request(struct party_node **head, char *item, double price, char *ta){
     // Check if item is "IDE"
     char* IDE = "IDE";
 
-    int count = 0;
-    for (int i = 0; i<4; i++){
-        if (item[i] == IDE[i]){
-            count++;
-           }
-        else{
-            continue;
-        }
-    }
-    if (count == 4){
+    int compare = strcmp(IDE, item);
+    if (compare == 0) {
         return -1;
     }
+
     // Create new node otherwise
-    else{
-        struct party_node* new_head = (struct party_node*)malloc(sizeof(struct party_node));
-            new_head->next = *head; 
-            new_head->item = item;
-            new_head->price = price;
-            new_head->ta = ta;
-            *head = new_head;
-        return 0;
-    }
+    struct party_node* new_head = (struct party_node*)malloc(sizeof(struct party_node));
+        new_head->next = *head; 
+        new_head->item = (char *)malloc((strlen(item)+1) * sizeof(char));
+        strcpy(new_head->item, item);
+        new_head->price = price;
+        new_head->ta = (char *)malloc((strlen(ta)+1) * sizeof(char));
+        strcpy(new_head->ta, ta);
+        *head = new_head;
+    return 0;
 }
 
 
 // Remove the last item added
 void remove_request(struct party_node **head){
-    
-    struct party_node *temp = *head;
-    *head = (*head)->next;
-    free(temp);
+    if (*head != NULL){
+        struct party_node *temp = *head;
+        *head = (*head)->next;
+        delete_node(temp);
+    }
 }
 
 
@@ -66,18 +82,6 @@ void make_sorted(struct party_node **head){
         *head = previous.next; // Make head point to the top of the stack again
     }
 }
-
-
-// Determine the length of a linked list
-int length_of_list(struct party_node **head){
-    int counter = 0;
-    struct party_node* temp_node = *head;
-    while (temp_node != NULL) {
-        counter++;
-        temp_node = temp_node->next;
-    }
-    return counter;
-}
     
 
 // Trim list to fit the budget
@@ -95,9 +99,9 @@ double finalize_list(struct party_node **head, double budget){
             temp_node = temp_node->next; // Iterate to consider next node            
         }
         else{
-            struct party_node *delete_node = temp_node->next;
-            temp_node->next = delete_node->next; // Automatically iterates to next node
-            free(delete_node);
+            struct party_node *node_to_delete = temp_node->next;
+            temp_node->next = node_to_delete->next; // Automatically iterates to next node
+            delete_node(node_to_delete);
         }
     }
     *head = previous.next;
