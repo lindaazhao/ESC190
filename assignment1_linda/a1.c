bcc95e378efcab0cf0c35c9d85be7b5c9d6b6996
lindaazhao <linda.zhao23@gmail.com>
2022-02-28 22:10:09 -0500

    Remove print statements & shorten load_menu()

--- assignment1_linda/a1.c
+++ assignment1_linda/a1.c
@@ -9,65 +9,49 @@ Menu* load_menu(char* fname){
 	// Create new menu struct
 	struct Menu* menu = (struct Menu*)malloc(sizeof(struct Menu));
 	menu->num_items = 0;
-	// fprintf(stdout, "menu->num_items: %d\n", menu->num_items);
+
 	// Initialize arrays for menu items
-	menu->item_codes = (char**)malloc(sizeof(char*)); // Allocate size of one pointer (to a string)
-	menu->item_names = (char**)malloc(sizeof(char*)); // Allocate size of one pointer (to a string)
-	menu->item_cost_per_unit = (double*)malloc(sizeof(double));
+	menu->item_codes = (char**)malloc(sizeof(char*)); // Allocate size of one pointer (for a string)
+	menu->item_names = (char**)malloc(sizeof(char*)); // Allocate size of one pointer (for a string)
+	menu->item_cost_per_unit = (double*)malloc(sizeof(double)); // Allocate size of one pointer (to a double)
 
 	// Initialize getline parameters
 	FILE* menu_file;
 	char* line = NULL;	
 	size_t len = 0;
-	ssize_t read;
 
-	menu_file = fopen(fname, "r");
+	menu_file = fopen(fname, "r"); // Open menu file
 	
 	// Get each line from the menu file, one by one
-	while ((read = getline(&line, &len, menu_file)) != -1){		
+	while (getline(&line, &len, menu_file) != -1){		
 		// Split line into item code, item name, and item price
 		char* token = strtok(line, MENU_DELIM);
 		char* item_code = strdup(token); // Remember to free these, since they are allocated using malloc
-		// fprintf(stdout, "Item Code Token: %s\n", item_code);
 		
 		token = strtok(NULL, MENU_DELIM);
 		char* item_name = strdup(token); // Remember to free these, since they are allocated using malloc
-		// fprintf(stdout, "Item Name Token: %s\n", item_name);
 		
 		token = strtok(NULL, "\n");
 		char* item_price_str = strdup(token); // Remember to free these, since they are allocated using malloc
-		// fprintf(stdout, "Item Price Token: %s\n", item_price_str);
+		double item_price = strtod(item_price_str+1, NULL); // strtod converts string to double value, item_price_str+1 to ignore the $
 
-		// fprintf(stdout, "Item Code: %s\n", item_code);
-		// fprintf(stdout, "Item Name: %s\n", item_name);
-		// fprintf(stdout, "Item Price Token+1: %s\n", item_price_str+1);
-
-		double item_price;
-		item_price = strtod(item_price_str+1, NULL); // strtod converts string to double value
-
-		// print_menu(menu);
-
+		// Increment number of items in the menu for each loop
 		menu->num_items++;
 
-		if (menu->num_items == 1) {
-			menu->item_codes[menu->num_items-1] = item_code;
-			menu->item_names[menu->num_items-1] = item_name;
-			menu->item_cost_per_unit[menu->num_items-1] = item_price;
-			// print_menu(menu);
-		}
-		else {			
-			// Make space for the new item by reallocating memory
+		// First item already has spaced allocated for it, only need to realloc if there are 2+ items
+		if (menu->num_items > 1) {
+			// Make space for the new item in the menu
 			menu->item_codes = (char**)realloc(menu->item_codes, (menu->num_items) * sizeof(char*));
 			menu->item_names = (char**)realloc(menu->item_names, (menu->num_items) * sizeof(char*));
 			menu->item_cost_per_unit = (double*)realloc(menu->item_cost_per_unit, (menu->num_items) * sizeof(double));
-			
-			// Add the item to the menu
-			menu->item_codes[menu->num_items-1] = item_code;
-			menu->item_names[menu->num_items-1] = item_name;
-			menu->item_cost_per_unit[menu->num_items-1] = item_price;
-			// print_menu(menu);
-		}	
-		free(item_price_str);	
+		}
+
+		// Update menu with the new item
+		menu->item_codes[menu->num_items-1] = item_code;
+		menu->item_names[menu->num_items-1] = item_name;
+		menu->item_cost_per_unit[menu->num_items-1] = item_price;	
+		
+		free(item_price_str); // Free the pointer that isn't used in the menu
 	}
 	
 	free(line);
@@ -203,23 +187,23 @@ void print_order(Order* order){
 }
 
 
-// void print_receipt(Order* order, Menu* menu){
-// 	for (int i = 0; i < order->num_items; i++){
-// 		double item_cost = get_item_cost(order->item_codes[i], menu);
-// 		fprintf(
-// 			stdout, 
-// 			"%d x (%s)\n @$%.2f ea \t %.2f\n", 
-// 			order->item_quantities[i],
-// 			order->item_codes[i], 
-// 			item_cost,
-// 			item_cost * order->item_quantities[i]
-// 		);
-// 	}
-// 	double order_subtotal = get_order_subtotal(order, menu);
-// 	double order_total = get_order_total(order, menu);
+void print_receipt(Order* order, Menu* menu){
+	for (int i = 0; i < order->num_items; i++){
+		double item_cost = get_item_cost(order->item_codes[i], menu);
+		fprintf(
+			stdout, 
+			"%d x (%s)\n @$%.2f ea \t %.2f\n", 
+			order->item_quantities[i],
+			order->item_codes[i], 
+			item_cost,
+			item_cost * order->item_quantities[i]
+		);
+	}
+	double order_subtotal = get_order_subtotal(order, menu);
+	double order_total = get_order_total(order, menu);
 	
-// 	fprintf(stdout, "Subtotal: \t %.2f\n", order_subtotal);
-// 	fprintf(stdout, "               -------\n");
-// 	fprintf(stdout, "Tax %d%%: \t$%.2f\n", TAX_RATE, order_total);
-// 	fprintf(stdout, "              ========\n");
-// }
+	fprintf(stdout, "Subtotal: \t %.2f\n", order_subtotal);
+	fprintf(stdout, "               -------\n");
+	fprintf(stdout, "Tax %d%%: \t$%.2f\n", TAX_RATE, order_total);
+	fprintf(stdout, "              ========\n");
+}
