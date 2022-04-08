a08b6abd5adad94642da595ea83cd7810c40fd62
Linda Zhao <linda.zhao23@gmail.com>
2022-04-07 20:31:56 -0400

    Workin on A2

--- assignment2/a2.c
+++ assignment2/a2.c
@@ -1,4 +1,5 @@
 #include "a2.h"
+#include <math.h>
 
 int bitwise_xor(int value){
     int key = (int)(KEY);
@@ -6,22 +7,39 @@ int bitwise_xor(int value){
 }
 
 char *xor_encrypt(char c){
-    char* bin_encrypt[7];
+    char* bin_encrypt = malloc(sizeof(char) * 7);
     
     int ascii_char = (int)(c);
+    printf("ascii_char: %d\n", ascii_char);
     int ascii_encrypt = bitwise_xor(ascii_char);
+    printf("ascii_encrypt: %d\n", ascii_encrypt);
 
     for (int i = 6; i >= 0; i--){
         if (ascii_encrypt - pow(2, i) >= 0){
-            bin_encrypt[6 - i] = 1;
+            bin_encrypt[6-i] = '1';
             ascii_encrypt = ascii_encrypt - pow(2, i);
+        }
+        else{
+            bin_encrypt[6-i] = '0';
         }
     }
     return bin_encrypt;
 }
 
 char xor_decrypt(char *s){
-    //add code here
+    char decrypted;
+    int ascii_char = 0;
+
+    // Convert binary string representation into an ASCII value
+    for (int i = 0; i < 7; i++){
+        if (s[i] == '1'){
+            ascii_char = ascii_char + pow(2, 6-i);
+        }
+    }
+
+    int ascii_decrypt = bitwise_xor(ascii_char);
+    decrypted = (char)(ascii_decrypt);
+    return decrypted;
 }
 
 char *gen_code(char *msg){
